f9561536089de345799bafb851c59b385bbf3576
liulun <xland@live.cn>
2020-08-22 14:15:20 +0800

    test code

--- test/test.js
+++ test/test.js
@@ -1,41 +1,65 @@
 const fs = require("fs-extra");
+const os = require("os");
 const path = require("path");
 const execa = require("execa");
+const { createServer } = require("vite");
+const { resolve } = require("path");
+const argv = require("minimist")(process.argv.slice(2));
 jest.setTimeout(100000);
 let devServer;
 beforeAll(async () => {
+  console.log("create server");
   let demoDir = path.join(__dirname, "demo");
-  let viteBin = path.join(__dirname, "../node_modules/vite/bin/vite.js");
   await execa("yarn", { cwd: demoDir });
-  await new Promise((resolve) => {
-    devServer = execa("vite", {
-      cwd: demoDir,
-    });
-    devServer.stdout.on("data", (data) => {
-      if (data.toString().match("running")) {
-        console.log("dev server running.");
-        resolve();
-      }
+  const options = { root: demoDir };
+  console.log("create server");
+  devServer = createServer(options);
+  let port = options.port || 3000;
+  devServer.on("error", (e) => {
+    if (e.code === "EADDRINUSE") {
+      console.log(`Port ${port} is in use, trying another one...`);
+      setTimeout(() => {
+        devServer.close();
+        devServer.listen(++port);
+      }, 100);
+    } else {
+      console.error(chalk.red(`[vite] server error:`));
+      console.error(e);
+    }
+  });
+  return new Promise((resolve) => {
+    devServer.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+      resolve();
     });
   });
-  console.log("ok");
+
+  // await execa("yarn", { cwd: demoDir });
+  // await new Promise((resolve) => {
+  //   devServer = execa(viteBin, { cwd: demoDir });
+  //   devServer.stderr.on("data", (data) => {
+  //     console.error(data.toString());
+  //     resolve();
+  //   });
+  //   devServer.stdout.on("data", (data) => {
+  //     if (data.toString().match("running")) {
+  //       console.log("dev server running.");
+  //       resolve();
+  //     }
+  //   });
+  // });
+  // console.log("ok");
 });
 afterAll(async () => {
-  //   if (browser) await browser.close();
-  if (devServer) {
-    devServer.kill("SIGTERM", {
-      forceKillAfterTimeout: 2000,
-    });
-  }
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      devServer.close();
+      resolve();
+    }, 66000)
+  );
 });
 describe("vite-electron", () => {
-  beforeAll(() => {
-    console.log("allen");
-  });
   describe("build", () => {
-    console.log("build");
-    test("test", () => {
-      console.log(123);
-    });
+    test("test", () => {});
   });
 });
